b9f23864c2769e179ebf155170e49affa65dde74
Luciano Nooijen <luciano@lucianonooijen.com>
2024-11-13 00:26:56 +0100

    Add Mermaid support

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  themes: ['@docusaurus/theme-mermaid'],
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -29,6 +30,10 @@ const config: Config = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
+  },
+
+  markdown: {
+    mermaid: true,
   },
 
   presets: [
